9aef60f202f36f91bba2f9c827e0ead7abe1253f
cryssw17 <cawash17@gmail.com>
2026-04-07 14:49:09 -0400

    about me page

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -2,11 +2,32 @@
 //Exercise: Build an "About Me" Component in this file
 
 export default function StudentWork() {
-  //add variables here
+  const name = 'Crystal';
+  const age = 34;
+  const hobbies = [
+    'reading',
+    'video games',
+    'photography',
+    'crochet',
+    'DIY craft kits',
+  ];
   return (
     <div>
-      {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>Who Am I?</h1>
+      <p>
+        Hi, my name is {name} and I am a {age} year old aspiring web developer
+        living on the East Coast. My college years were spent hanging out on
+        boats, at beaches, and trekking through salt marshes studying marine
+        biology. Most of my career has been in customer service, but my
+        curiosity and urge to create inspired me to begin this transition into a
+        career in tech. I love learning new skills, figuring out how things
+        work, and enjoy building/fixing things. Some of my hobbies include:{' '}
+      </p>
+      <ul>
+        {hobbies.map((hobby, index) => (
+          <li key={index}>{hobby}</li>
+        ))}
+      </ul>
     </div>
   );
 }
