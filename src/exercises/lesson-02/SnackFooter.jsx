7241e6e151b7710a6f34d5e8cacf9d497685db9c
cryssw17 <cawash17@gmail.com>
2026-04-14 21:16:57 -0400

    exercise 2, app to rank favorite snacks

---
+++ src/exercises/lesson-02/SnackFooter.jsx
@@ -0,0 +1,7 @@
+export default function SnackFooter() {
+  return (
+    <div>
+      <p>Do you agree with my ranking?</p>
+    </div>
+  );
+}
---
+++ src/exercises/lesson-02/SnackHeader.jsx
@@ -0,0 +1,7 @@
+export default function SnackHeader() {
+  return (
+    <div>
+      <h1>My Favorite Snacks Ranked</h1>
+    </div>
+  );
+}
